630ad4e022eda961feac846d2ec6bc23efd7d04f
Lee Comstock <lee_comstock@hotmail.com>
2019-08-31 21:00:43 +0200

    uploaded react minesweeper build folder

---
+++ build/precache-manifest.f078006256e954466e8642d166817e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4b92efc41871f35e54146e13e69221c",
+    "url": "/static/media/DSC_0673.c4b92efc.JPG"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3c77110cc711cb4c54e",
+    "url": "/static/js/main.2814d35f.chunk.js"
+  },
+  {
+    "revision": "6aa3816f492834539a41",
+    "url": "/static/js/2.b19382f7.chunk.js"
+  },
+  {
+    "revision": "e3c77110cc711cb4c54e",
+    "url": "/static/css/main.e256a19a.chunk.css"
+  },
+  {
+    "revision": "a4c2fd8ccbcb69388d51c46a588c69a8",
+    "url": "/index.html"
+  }
+];
